984cb6e11352f30506bcb3f8972422d8ae821548
alex0328 <lukasz.szlaszynski@gmail.com>
2020-03-01 19:00:28 +0100

    prepare for heroku

--- cut_project/cut_project/settings.py
+++ cut_project/cut_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -77,10 +78,10 @@ WSGI_APPLICATION = 'cut_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'NAME': 'cutmyurl', ## nazwa bazy danych
+        'NAME': 'cut', ## nazwa bazy danych
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'cutter',
-        'PASSWORD': 'Japierdziu0328',
+        'USER': 'database',
+        'PASSWORD': 'localhost',
         'HOST': 'localhost'
     }
 }
